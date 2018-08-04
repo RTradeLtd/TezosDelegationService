5e9e1e74eaafb4ed8b355b3f28d14935b5fc4bc7
potsables <alextrottier2014@gmail.com>
2018-08-04 13:45:23 -0700

    updated install script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TezosDelegationService
+
 repo for our tezos delegation service
+
+## Contents
+
+## Scripts
+
+* `tezos_install.sh`
+  * Used to install tezos node software
---
+++ Scripts/tezos_install.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+  
+# Please answer yes to any of the prompts
+# Modified the script listed here http://tezos.gitlab.io/master/introduction/howtoget.html#build-from-sources as it was out of date
+# Built binaries will be in the root directory of the git repo that we clone
+# Tested On:
+#  -> Ubuntu 18.04
+#  -> Ubuntu 16.04
+#  -> Pop!_OS 18.04
+
+## Check for ubuntu 16.04 an dinstall bubble wrap
+DISTRIB_RELEAS=$(grep "DISTRIB_RELEASE" /etc/lsb-release | awk -F '=' '{print $2}')
+if [[ DISTRIB_RELEASE == "16.04" ]]; then
+    sudo add-apt-repository ppa:ansible/bubblewrap -y
+    sudo add-apt-repository ppa:git-core/ppa -y
+    sudo apt-get update -y
+fi
+
+sudo apt install -y git m4 build-essential patch unzip bubblewrap wget make gcc m4 g++ aspcud curl bzip2 rsync
+wget https://github.com/ocaml/opam/releases/download/2.0.0-rc4/opam-2.0.0-rc4-x86_64-linux
+sudo cp opam-*linux /usr/local/bin/opam
+sudo chmod a+x /usr/local/bin/opam
+git clone https://gitlab.com/tezos/tezos.git
+cd tezos
+git checkout betanet
+opam init --bare
+make build-deps
+eval $(opam env)
+make
+export PATH=~/tezos:$PATH
+source ./src/bin_client/bash-completion.sh
+export TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER=Y
